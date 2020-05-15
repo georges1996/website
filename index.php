212e309f306c21389949baa003f6183176e7016c
George Schena <george@madetech.com>
2020-05-15 19:01:57 +0100

    Add link styles

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="index.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+<script src="https://use.fontawesome.com/1fe55a52b0.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -8,7 +9,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand text-color-2" href="/">George Schena</a>
+  <a class="navbar-brand text-color-2" href="/">George Schena</a>
 </nav>
 
 <div class="position-relative overflow-hidden p-md-1 text-center jumbotron-dark-blue-background">
@@ -16,6 +17,11 @@
     <h1 class="display-4 font-weight-normal text-color-1">Hey I'm George Schena</h1>
     <p class="lead font-weight-normal text-color-1">Documenting my career through the technology industry</p>
     <a class="btn btn-outline-secondary" href="mailto:georgeschena@hotmail.com">Contact me</a>
+    <div class="pt-3">
+      <a class="fa fa-github fa-2x icon-colour pr-2" aria-hidden="true" href="https://github.com/georgeschena" target="_blank"></a>
+      <a class="fa fa-linkedin-square fa-2x icon-colour pr-2" aria-hidden="true" href="https://www.linkedin.com/in/george-schena/" target="_blank"></a>
+      <a class="fa fa-instagram fa-2x icon-colour" aria-hidden="true" href="https://www.instagram.com/georgeschena/" target="_blank"></a>
+    </div>
   </div>
   <div class="product-device shadow-sm d-none d-md-block"></div>
   <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
